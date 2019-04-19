b88ae3abad73419e09bdcd5edc6b1dfb52630732
wv1344 <wv1344@gmail.com>
2019-04-19 15:59:13 +0800

    add node

--- WJ/wxapp/lol/pages/legends/legends.js
+++ WJ/wxapp/lol/pages/legends/legends.js
@@ -1,22 +1,30 @@
 Page({
-  data:{
-    legends:[
-      {
-        name:'阿卡丽',
-        title:'刺客'
-      },
-      {
-        name:'盖伦',
-        title:'战士'
-      },
-      {
-        name:'亚索',
-        title:'风男'
-      },
-      {
-        name:'火男',
-        title:'法师'
-      }
-    ]
+  data: {
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4],
+    legends: [{
+      name: '阿卡丽',
+      title: '刺客'
+    },
+    {
+      name: '盖伦',
+      title: '战士'
+    },
+    {
+      name: '亚索',
+      title: '风男'
+    },
+    {
+      name: '火男',
+      title: '法师'
+    }
+  ],
   }
 })
