060f51526dda69736af0a284d2de36879778d107
wv1344 <wv1344@gmail.com>
2019-04-24 17:04:10 +0800

    wxapp

--- WJ/wxapp/wxappLearn/pages/main/index.js
+++ WJ/wxapp/wxappLearn/pages/main/index.js
@@ -1,74 +1,30 @@
-// pages/main/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    city: '南昌',
     imgUrls: [
       'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000
+    items:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(1);
+  onLoad(){
+    let that = this;
+    wx.request({
+      url:'https://www.easy-mock.com/mock/5cbf0084a70f960bc333c4a4/wewe',
+      success:function(res){
+        // console.log(res);
+        that.setData({
+          items:res.data.data.movieList
+        });
+        console.log(that.data.items);
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    console.log(2);
+  onPullDownRefresh(){
+    console.log('下拉');
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    console.log(3);
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    console.log(4);
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    console.log(5);
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    console.log(6);
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    console.log(7);
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    console.log(8);
+  onReachBottom(){
+    console.log('666')
   }
 })
