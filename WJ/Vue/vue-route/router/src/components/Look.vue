e527c3b034f4fc4ac42012b4c8373cec638cefd4
wv1344 <wv1344@gmail.com>
2019-05-31 17:01:35 +0800

    vue-route

---
+++ WJ/Vue/vue-route/router/src/components/Look.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Look">
+    {{ msg }}
+    {{ $route.params.id }}
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Look',
+  data () {
+    return {
+      msg: '看一看'
+    }
+  }
+}
+</script>
