f039fc14c9bae2cb745d64f32bd13a176bbc80ce
wv1344 <wv1344@gmail.com>
2019-06-14 12:57:41 +0800

    awesomePos

---
+++ WJ/Vue/self/awesomePos/src/components/common/leftNav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="left-nav">
+    <ul>
+      <li>
+        <i class="icon iconfont icon-dingdan"></i>
+        <div>收银</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-shouye"></i>
+        <div>店铺</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-xiaoshi"></i>
+        <div>商品</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-yingchengka"></i>
+        <div>会员</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-shezhi"></i>
+        <div>设置</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .left-nav{
+    color: #ffffff;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1d8ce0;
+    float: left;
+    width: 5%;
+  }
+  .iconfont{
+    font-size: 24px;
+  }
+  .left-nav ul{
+    padding: 0;
+    margin: 0;
+  }
+  .left-nav li{
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid skyblue;
+    padding: 10px;
+  }
+</style>
