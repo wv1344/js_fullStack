8c0460122a16c4d965db60c43af125d7e774a78a
wv1344 <wv1344@gmail.com>
2019-06-15 21:39:15 +0800

    element

---
+++ WJ/Vue/todomvc/src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Clicked: {{count}} times </h1><br>
+    <button @click="increment">ADD</button>
+  </div>
+</template>
+
+<script>
+// 让store里的改变状态的方法 mapActions
+import {mapActions,mapMutations} from 'vuex'
+export default {
+  // props:['count'],
+  // 自身的状态
+  data () {
+    return {
+      // count:this.$store.state.count
+    }
+  },
+  methods:{
+    ...mapActions(['increment']),
+    ...mapMutations(['increment'])
+  },
+  computed: {
+    count(){
+      return this.$store.state.count;
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ WJ/Vue/todomvc/src/components/CounterBrother.vue
@@ -0,0 +1,30 @@
+  <template>
+  <div>
+    <h1>{{count}}</h1>
+  </div>
+</template>
+  
+  <script>
+import { mapActions, mapMutations } from "vuex";
+export default {
+  // props:['count']
+  data() {
+    return {
+      // count: this.$store.state.count
+    };
+  },
+  methods: {
+    ...mapActions(["increment"]),
+    ...mapMutations(["increment"])
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+};
+</script>
+  
+  <style>
+</style>
+  
