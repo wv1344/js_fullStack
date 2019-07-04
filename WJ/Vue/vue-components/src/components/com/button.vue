a9b42329f855228ef717799c28abb24db8c9364e
wv1344 <wv1344@gmail.com>
2019-07-04 16:56:55 +0800

    vue

---
+++ WJ/Vue/vue-components/src/components/com/button.vue
@@ -0,0 +1,61 @@
+<template>
+  <button :class="'i-button-size' + size" :disable="disable" @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  function oneOf(value,validList){
+    for(let i = 0;i<validList.length;i++){
+      if(value === validList[i]){
+        return true
+      }
+    }
+    return false
+  }
+import Bus from '@/bus/bus'
+
+  export default{
+    props:{
+      size:{
+        validator(value) {
+          return oneOf(value,['small','large','default'])
+        },
+        default:'default'
+      },
+      disable:{
+        type:Boolean,
+        default:false
+      }
+    },
+    data(){
+      return {
+        msg:'老师的会计分录'
+      }
+    },
+    methods: {
+      handleClick (){
+        // console.log(this.msg)
+        // this.$emit('on-click',this.msg)
+        Bus.$emit('todo',this.msg)
+      }
+    },
+  }
+</script>
+
+<style scoped>
+button{
+  border-radius: 5px;
+  cursor: pointer;
+  outline: none;
+}
+.i-button-sizelarge{
+  padding: 12px;
+}
+.i-button-sizedefault{
+  padding: 8px;
+}
+.i-button-sizesmall{
+  padding: 4px;
+}
+</style>
