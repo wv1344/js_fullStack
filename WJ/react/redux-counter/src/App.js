3209949b8f459d45c550cf768e068be0bcbdf591
wv1344 <wv1344@gmail.com>
2019-07-16 17:07:45 +0800

    redux

--- WJ/react/redux-counter/src/App.js
+++ WJ/react/redux-counter/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Count extends React.Component{
+  state = {}
+  handleIn(){
+    this.props.increment();
+  //   store.dispatch({type:'INCREMENT'})
+  }
+  handleDe(){
+    this.props.decrement();
+  //   store.dispatch({type:'DECREMENT'})
+  }
+  handleAddFilm(){
+    this.props.addFilm();
+  //   store.dispatch({
+  //     type:'ADD_FILM',
+  //     film:{name:'狮子王'}
+  //   })
+  }
+  render(){
+    console.log(this.props)
+    const { count,films } = this.props
+    return (
+      <div>
+        count:{count}
+        
+        <button onClick={this.handleIn.bind(this)}>+</button>
+        <button onClick={this.handleDe.bind(this)}>-</button>
+        <button onClick={this.handleAddFilm.bind(this)}>add film</button>
+        {films.map((film,i) => {
+          return (
+            <li key={i}>
+              {film.name}
+            </li>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count:state.count,
+    films:state.films
+  }
+}
+
+const mapDispatchProps = (dispatch) => {
+  return {
+    foo:()=> {console.log('foo')},
+    increment:()=>{dispatch({type:'INCREMENT'})},
+    decrement:()=>{dispatch({type:'DEINCREMENT'})},
+    addFilm:()=> {
+    dispatch({
+      type:'ADD_FILM',
+      film:{name:'123'}
+    })
+  }
+ }
+}
+export default connect(mapStateToProps,mapDispatchProps)(Count);
