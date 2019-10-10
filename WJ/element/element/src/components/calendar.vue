bd89b10f5f1d6891d9abcbada18afc92add607cf
wangji.wj <wangji.wj@120yibao.com>
2019-10-10 11:05:54 +0800

    日历组件

--- WJ/element/element/src/components/calendar.vue
+++ WJ/element/element/src/components/calendar.vue
@@ -14,13 +14,13 @@
       </div>
       <div class="date">
         <div class="before-empty grid" v-for="(item,index) in beforeEmptyDay" :key="index+'before'" @click="handleBeforeClick(item)">
-          <div class="wrap">{{item}}</div>
+          <div class="wrap"><slot :data="item"><div>{{item.day}}</div></slot></div>
         </div>
         <div class="this-month-days grid" :class="selectFormat === year+'-'+item.monthFormat+'-'+item.dateFormat ? 'selected' : '' " @click="handleClick(item)"  v-for="(item,index_mo) in thisMonthDays" :key="index_mo">
-          <div class="wrap" :class="format === year+'-'+item.monthFormat+'-'+item.dateFormat ? 'today' : '' ">{{item.date}}</div>
+          <div class="wrap" :class="format === year+'-'+item.monthFormat+'-'+item.dateFormat ? 'today' : '' "><slot :data="item"><div>{{item.day}}</div></slot></div>
         </div>
         <div class="after-empty grid" v-for="(item,index) in afterEmptyDay" :key="index+'after'" @click="handleAfterClick(item)">
-          <div class="wrap">{{item}}</div>
+          <div class="wrap">{{item.day}}</div>
         </div>
       </div>
     </div>
@@ -31,7 +31,11 @@
 export default {
   name: 'Calendar',
   props: {
-    value: [Date, String, Number]
+    value: [Date, String, Number],
+    range: {
+      type: Array,
+      default: null
+    }
   },
   data () {
     return {
@@ -53,23 +57,28 @@ export default {
     }
   },
   methods: {
+    // 个位数前面加 0
     zero (i) {
       return i >= 10 ? i : '0' + i
     },
+    // 获取当前月的天数
     getThisMonthDays (ye, mon) {
       return new Date(ye, mon, 0).getDate()
     },
+    // 创建本月日期
     createDays (year, month) {
       this.thisMonthDays = []
       let days = this.getThisMonthDays(year, month)
       for (let i = 1; i <= days; i++) {
         this.thisMonthDays.push({
-          date: i,
+          day: i,
+          month: this.zero(month),
           dateFormat: this.zero(i),
           monthFormat: this.zero(month)
         })
       }
     },
+    // 创建上个月和下个月日期格子
     emptyDays (year, month) {
       this.beforeEmptyDay = []
       this.afterEmptyDay = []
@@ -79,16 +88,24 @@ export default {
       } else {
         this.emptyDay = (week === 1 ? 7 : week - 1)
       }
+      // 上个月有几个格子
       let preMonthDays = month - 1 < 0 ? this.getThisMonthDays(year - 1, 12) : this.getThisMonthDays(year, month - 1)
-
       for (let i = 1; i <= this.emptyDay; i++) {
-        this.beforeEmptyDay.push(preMonthDays - (this.emptyDay - i))
+        this.beforeEmptyDay.push({
+          day: preMonthDays - (this.emptyDay - i),
+          month: this.zero(month)
+        })
       }
+      // 下个月有几个格子
       let after = (42 - this.getThisMonthDays(year, month) - this.emptyDay)
       for (let i = 1; i <= after; i++) {
-        this.afterEmptyDay.push(i)
+        this.afterEmptyDay.push({
+          day: i,
+          month: this.zero(month)
+        })
       }
     },
+    // 今天
     today () {
       this.year = new Date().getUTCFullYear()
       this.month = new Date().getUTCMonth() + 1
@@ -97,11 +114,13 @@ export default {
       this.format = this.year + '-' + this.zero(this.month) + '-' + this.zero(this.date)
       this.selectFormat = this.year + '-' + this.zero(this.month) + '-' + this.zero(this.date)
     },
+    // 渲染日历
     display (year, month, date) {
       this.emptyDays(year, month)
       this.createDays(year, month)
     },
     handleClick (e) {
+      // e.selected = 'selected'
       this.selectFormat = this.year + '-' + e.monthFormat + '-' + e.dateFormat
     },
     handleBeforeClick (e) {
@@ -149,10 +168,16 @@ export default {
         this.selectFormat = this.year + '-' + this.zero(this.month) + '-' + this.zero(this.date)
         this.display(this.year, this.month, this.date)
       } else if (Object.prototype.toString.call(this.value) === '[object String]') {
-        console.log(this.value)
+        console.log(this.value.split('/'))
       }
     } else {
       this.today()
+    }
+    if (this.range) {
+      if (new Date(this.range[0]).getDay() === 1 && new Date(this.range[1]).getDay() === 0 && new Date(this.range[1]).getMonth() - new Date(this.range[0]).getMonth() <= 1) {
+        // console.log(new Date(this.range[0]).getDay())
+
+      }
     }
   },
   watch: {
